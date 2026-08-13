a4d068fdf090ad03893d29431773a74a5806afc0
galadmitro <galadmitro87392824@gmail.com>
2026-08-13 23:42:16 +0300

    Update ExampleMod.java

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,14 +1,36 @@
 package com.example.examplemod;
 
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.config.ModConfig;
+import net.neoforged.neoforge.client.event.RegisterKeyMappingsEvent;
+import org.lwjgl.glfw.GLFW;
 
 @Mod("examplemod")
 public class ExampleMod {
 
+    public static final KeyMapping TOGGLE_NAMETAG_KEY = new KeyMapping(
+            "key.examplemod.toggle_nametag",
+            InputConstants.Type.KEYSYM,
+            GLFW.GLFW_KEY_O,
+            "key.categories.examplemod"
+    );
+
     public ExampleMod(ModContainer modContainer) {
         // Register Client Config for Mods -> Configure menu
         modContainer.registerConfig(ModConfig.Type.CLIENT, Config.SPEC);
+    }
+
+    @EventBusSubscriber(modid = "examplemod", bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModBusEvents {
+        @SubscribeEvent
+        public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
+            event.register(TOGGLE_NAMETAG_KEY);
+        }
     }
 }
