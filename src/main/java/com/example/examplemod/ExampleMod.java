636f6827c42d12eb214bf2500a43e79be73d660f
galadmitro <galadmitro87392824@gmail.com>
2026-08-13 19:39:44 +0300

    ah

---
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -0,0 +1,13 @@
+package com.example.examplemod;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+
+@Mod(ExampleMod.MOD_ID)
+public class ExampleMod {
+    public static final String MOD_ID = "examplemod";
+
+    public ExampleMod(IEventBus modEventBus) {
+        // Core initialization logic
+    }
+}
